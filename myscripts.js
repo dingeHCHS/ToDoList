fe16dc4e9f62ba0c0d5d9b6e96c930d4b1aa7cd5
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-04-26 11:40:26 -0500

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,23 @@
 
+document.getElementById("addBtn").addEventListener("click", addText);
+
+
+function addText(){
+  //Line Item
+  var listItem = document.createElement("li");
+  listItem.innerHTML = document.getElementById("myInput").value;
+  document.getElementById("myUl").appendChild(listItem);
+  
+  //Delete Button
+  var deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = "x";
+  deleteBtn.className = "delete";
+  deleteBtn.onclick = removeText;
+  listItem.appendChild(deleteBtn);
+}
+
+function removeText(){
+  var listItem = this.parentNode;
+  var ul = listItem.parentNode;
+  ul.removeChild(listItem);
+}
